c957794f4a5aa04cab00ee57c4afd526d42501e4
Coder-Matheo <ali.dinarvand1370@gmail.com>
2021-12-28 00:12:26 +0100

    1. login Fragment 2. design Login

---
+++ app/src/main/java/rob/netflix2app/MainActivity.java
@@ -0,0 +1,22 @@
+package rob.netflix2app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LoginFragment loginFragment = new LoginFragment();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, loginFragment).commit();
+    }
+
+}
